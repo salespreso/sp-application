a0659df1f1fff8f8a456089b4d17938d37052fd5
Adam Hoddinott <adam@messymind.net>
2015-08-04 13:48:31 +1000

    chore(example): create one

--- example/js/index.js
+++ example/js/index.js
@@ -1,5 +1,77 @@
-import { Index } from "lib/index";
+import React from "react";
+import {Route} from "react-router";
+import {ApplicationRunner} from "lib/runner";
+import {parent, application} from "lib/decorators";
 
-console.log("Works!");
+import {Log} from "sp-log";
+let log = Log("example");
 
-export default {};
+@application()
+class ParentApp {
+	static get routes() {
+		return (
+			<Route name="foo" />
+		);
+	}
+
+	static get store() {
+		return {
+			foo: true,
+			bar: false,
+			baz: {
+				hello: true
+			}
+		};
+	}
+
+	static get facets() {
+		return {
+			foo: {
+				cursors: [],
+				get() {
+					return "hello";
+				}
+			},
+			another: {
+				cursors: ["hello"],
+				get() {
+					return "nothello";
+				}
+			}
+		};
+	}
+}
+
+@parent("parentapp")
+@application()
+class ChildApp {
+	static get routes() {
+		return (
+			<Route name="bar" />
+		);
+	}
+
+	static get store() {
+		return {
+			testStore: {
+				foo: true,
+				bar: "baz"
+			}
+		};
+	}
+
+	static get facets() {
+		return {
+			hai: {
+				cursors: ["hai"],
+				get() {
+					return "hai";
+				}
+			}
+		};
+	}
+}
+
+log.info(`routes: ${JSON.stringify(ApplicationRunner.createRoutes(), null, 2)}`);
+log.info(`facets: ${JSON.stringify(ApplicationRunner.createFacets(), null, 2)}`);
+log.info(`stores: ${JSON.stringify(ApplicationRunner.createStore(), null, 2)}`);
