ab6cce57784f6257687e15106574e65200d1a735
Adam Hoddinott <adam@messymind.net>
2015-08-31 09:33:53 +1000

    feat(jspm): fix for latest version

--- config.js
+++ config.js
@@ -1,31 +1,29 @@
 System.config({
-  "transpiler": "babel",
-  "babelOptions": {
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  baseURL: window.__karma__ ? "base" : "/",
+  babelOptions: {
     "blacklist": [],
     "optional": [
       "runtime"
     ],
     "stage": 0
   },
-  "paths": {
-    "*": "*.js",
-    "github:*": "jspm_packages/github/*.js",
-    "npm:*": "jspm_packages/npm/*.js"
-  }
-});
-
-System.config({
-  "map": {
-    "babel": "npm:babel-core@5.8.12",
-    "babel-runtime": "npm:babel-runtime@5.8.12",
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+  map: {
+    "babel": "npm:babel-core@5.8.23",
+    "babel-runtime": "npm:babel-runtime@5.8.20",
     "bluebird": "npm:bluebird@2.9.34",
-    "core-js": "npm:core-js@0.9.18",
-    "lodash": "npm:lodash@3.10.0",
+    "core-js": "npm:core-js@1.1.3",
+    "lodash": "npm:lodash@3.10.1",
     "react": "npm:react@0.13.3",
     "react-router": "npm:react-router@1.0.0-beta3",
     "sp-log": "npm:sp-log@1.1.7",
     "github:jspm/nodelibs-buffer@0.1.0": {
-      "buffer": "npm:buffer@3.3.1"
+      "buffer": "npm:buffer@3.4.3"
     },
     "github:jspm/nodelibs-events@0.1.1": {
       "events": "npm:events@1.0.2"
@@ -48,7 +46,7 @@ System.config({
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
-    "npm:babel-runtime@5.8.12": {
+    "npm:babel-runtime@5.8.20": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:bluebird@2.9.34": {
@@ -58,12 +56,12 @@ System.config({
       "andlog": "npm:andlog@1.0.0",
       "fs": "github:jspm/nodelibs-fs@0.1.2"
     },
-    "npm:buffer@3.3.1": {
+    "npm:buffer@3.4.3": {
       "base64-js": "npm:base64-js@0.0.8",
       "ieee754": "npm:ieee754@1.1.6",
       "is-array": "npm:is-array@1.0.1"
     },
-    "npm:core-js@0.9.18": {
+    "npm:core-js@1.1.3": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
@@ -95,7 +93,7 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.1",
       "source-map": "npm:source-map@0.1.31"
     },
-    "npm:lodash@3.10.0": {
+    "npm:lodash@3.10.1": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:path-browserify@0.0.0": {
@@ -119,10 +117,8 @@ System.config({
       "inherits": "npm:inherits@2.0.1",
       "isarray": "npm:isarray@0.0.1",
       "process": "github:jspm/nodelibs-process@0.1.1",
-      "stream": "github:jspm/nodelibs-stream@0.1.0",
       "stream-browserify": "npm:stream-browserify@1.0.0",
-      "string_decoder": "npm:string_decoder@0.10.31",
-      "util": "github:jspm/nodelibs-util@0.1.0"
+      "string_decoder": "npm:string_decoder@0.10.31"
     },
     "npm:source-map@0.1.31": {
       "amdefine": "npm:amdefine@1.0.0",
@@ -133,7 +129,7 @@ System.config({
     "npm:sp-log@1.1.7": {
       "bluebird": "npm:bluebird@2.9.34",
       "bows": "npm:bows@1.4.8",
-      "lodash": "npm:lodash@3.10.0"
+      "lodash": "npm:lodash@3.10.1"
     },
     "npm:stream-browserify@1.0.0": {
       "events": "github:jspm/nodelibs-events@0.1.1",
@@ -156,4 +152,3 @@ System.config({
     }
   }
 });
-
